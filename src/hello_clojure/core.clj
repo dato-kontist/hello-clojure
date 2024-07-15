6ad0db60f85e63fbb23ae16adc8c67c08b14d20d
Daniel Tostes <dato@ageras.com>
2024-07-15 14:15:55 +0200

    Update main to printHello
    - Remove unecessary code docs

--- src/hello_clojure/core.clj
+++ src/hello_clojure/core.clj
@@ -4,15 +4,16 @@
 (ns hello-clojure.core
   (:gen-class))
 
-;; This is a function definition. The -main function takes an optional list of arguments.
-(defn -main
-  ;; This is a docstring. It describes what the function does. (I wonder why should one clean code if they can explain all what the code does in documentation - irony)
-  "Prints a greeting message. Optionally takes a name as an argument."
-  ;; This is a vector of arguments. The & indicates that the function can take any number of arguments.
-  [& args]
-  ;; This is an if statement. It checks if the args vector is empty.
+(defn- printHello [args]
   (if (empty? args)
     ;; if true
     (println "Hello, World!")
     ;; "else?" false
     (println (str "Hello, " (first args) "!"))))
+
+;; This is a function definition. The -main function takes an optional list of arguments.
+(defn -main
+  ;; This is a vector of arguments. The & indicates that the function can take any number of arguments.
+  [& args]
+  ;; This is a function call.
+  (printHello args))
