34996964706641a62c05f618e75ddf3314c69adb
Daniel Tostes <dato@ageras.com>
2024-07-18 11:47:21 +0200

    Add register_user_use_case test

---
+++ test/register_user/register_user_use_case_test.clj
@@ -0,0 +1,23 @@
+(ns register-user.register-user-use-case-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [domain.ports :refer [UserRepositoryPort]]
+            [register-user.register-user-use-case :refer [register-user]]))
+
+(deftest register-user-tests
+  (testing "User already registered"
+    (let [mock-repo (reify UserRepositoryPort
+                      (find-by-email [_ _] true)
+                      (create [_ _] nil))]
+      (try
+        (register-user mock-repo {:email "test@example.com"})
+        (is false "Expected exception not thrown")
+        (catch clojure.lang.ExceptionInfo e
+          (is (= "User already registered. " (ex-message e)))
+          (is (= {:email "test@example.com"} (ex-data e)))))))
+
+  (testing "User registration successful"
+    (let [mock-repo (reify UserRepositoryPort
+                      (find-by-email [_ _] false)
+                      (create [_ user] user))]
+      (is (= {:email "newuser@example.com"}
+             (register-user mock-repo {:email "newuser@example.com"}))))))
