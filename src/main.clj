a504942b5ce521e82cbcb8e48310b0f769a3fd38
Daniel Tostes <dato@ageras.com>
2024-08-01 16:09:59 +0200

    Add /users resource
    - Enable both in-memory and psql adapters to work

--- src/main.clj
+++ src/main.clj
@@ -5,11 +5,27 @@
             [clojure.tools.logging :as log]
             [infrastructure.db :refer [db-connection]]
             [infrastructure.docker :refer [docker-postgres]]
-            [register-user.adapter.in-memory-user-repository :refer [create-in-memory-repository]]))
+            [register-user.adapter.in-memory-user-repository :refer [create-in-memory-repository]]
+            [register-user.adapter.psql-user-repository :refer [create-in-psql-user-repository]]
+            [ring.middleware.params :as params]
+            [ring.middleware.keyword-params :as keyword-params]
+            [ring.middleware.resource :as resource]
+            [ring.util.response :as response]))
 
 (def in-memory-user-db (atom {}))
 
 (def in-memory-user-repo (create-in-memory-repository in-memory-user-db))
+(def psql-user-repo (create-in-psql-user-repository db-connection))
+
+(def app
+  (-> (fn [request]
+        (case (:uri request)
+          "/users/in-memory" ((create-register-user-http-adapter in-memory-user-repo) request)
+          "/users/psql" ((create-register-user-http-adapter psql-user-repo) request)
+          (response/not-found "Not Found")))
+      (params/wrap-params)
+      (keyword-params/wrap-keyword-params)
+      (resource/wrap-resource "/")))
 
 (defn -main [& {:keys [join?] :or {join? true}}]
   (try
@@ -20,7 +36,8 @@
     (mount/start #'db-connection)
     (log/info "DB connection started.")
     (log/info "Starting Jetty server...")
-    (run-jetty (create-register-user-http-adapter in-memory-user-repo) {:port 3000 :join? join?})
+    (run-jetty app {:port 3000 :join? join?})
+
     (log/info "Jetty server started.")
     (catch Exception e
       (log/error e "Failed to start the application."))))
